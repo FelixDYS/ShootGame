ca63798b0a2cb97517842878764a8ad05317eeda
FelixDYS <846775462@qq.com>
2016-11-14 23:32:07 +0800

    敌人攻击

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,32 +4,81 @@ using System.Collections;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State
+    {
+        Idle,
+        Chasing,
+        Attacking
+    };
+    State currentState;
     NavMeshAgent pathfinder;
     Transform target;
+    float attackDistanceThreshold = 1.5f;
+    float timeBetweenAttacks = 1;
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
 
     protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
+        currentState = State.Chasing;
         target = (GameObject.FindGameObjectWithTag("Player")).transform;
 
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
         StartCoroutine(UpdatePath());
     }
 
     void Update()
     {
+        if (Time.time > nextAttackTime)
+        {
+            float sqrDestToTartget = (target.position - transform.position).sqrMagnitude;
+            if (sqrDestToTartget < Mathf.Pow(attackDistanceThreshold, 2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                //StartCoroutine(Attack());
+            }
+        }
+        
     }
 
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+        Vector3 originalPosition = transform.position;
+        Vector3 attackPosition = target.position;
+
+        float attackSpeed = 3;
+        float percent = 0;
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolatio = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolatio);
+            yield return null;
+        }
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
+    }
     IEnumerator UpdatePath()
     {
         float refreshRate = 0.25f;
         while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            if (!dead)
+            if (currentState == State.Chasing)
             {
-                pathfinder.SetDestination(targetPosition);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
             }
+            
             yield return new WaitForSeconds(refreshRate);
         }
     }
