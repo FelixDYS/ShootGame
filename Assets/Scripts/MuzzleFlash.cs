a7591ae8b11f48512f370fc00d0a21b594a759fd
FelixDYS <846775462@qq.com>
2016-12-01 21:31:36 +0800

    E16完成

---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuzzleFlash : MonoBehaviour
+{
+    public GameObject flashHolder;
+    public Sprite[] flashSprites;
+
+    public SpriteRenderer[] spriteRenders;
+    public float flashTime;
+
+    void Start()
+    {
+        Deactivate();
+    }
+    public void Activate()
+    {
+        flashHolder.SetActive(true);
+
+        int flashSpriteIndex = Random.Range(0, flashSprites.Length);
+        for (int i = 0; i < spriteRenders.Length; i++)
+        {
+            spriteRenders[i].sprite = flashSprites[flashSpriteIndex];
+        }
+        Invoke("Deactivate", flashTime);
+    }
+
+    void Deactivate()
+    {
+        flashHolder.SetActive(false);
+    }
+}
